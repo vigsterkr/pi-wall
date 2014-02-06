b7719bf0c05e3980dd372759d0c3cd367e62d6aa
Viktor Gal <viktor.gal@maeth.com>
2014-02-06 01:33:59 +0000

    Refactor player

--- src/player.py
+++ src/player.py
@@ -1,46 +1,66 @@
 #!/usr/bin/python
 
-from gi.repository import Gst
-from gi.repository import GstNet
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GstNet, GObject
+
+GObject.threads_init()
+Gst.init(None)
 
 class Player(object):
-	def __init__(self):
-		Gst.init (None)
-		self.player = Gst.ElementFactory.make("playbin", "Player")
+	def __init__(self, filepath):
+		self._filepath = filepath
+		self.player = get_pipeline()
 
 		self.bus = self.player.get_bus()
 		self.bus.add_signal_watch()
 	        self.bus.connect("message", self.on_bus_msg)
-       		self.player.connect("about-to-finish", self.about_to_finish)
+		
 		self._loop = False
+		
+		#self.player.props.uri = "file:///home/pi/Park_720p.mp4"
+		self.player.set_state(Gst.State.PAUSED)
 		
 		# bug ? as i get -1 for the standard
 		self.GST_CLOCK_TIME_NONE = 18446744073709551615
 
-		self._slave = False
-		self._master = False
+		self._prerolling = True
 
 	
-	def play(self, uri):
-		self._last_uri = uri
-		self.player.props.uri = self._last_uri
-		self.player.set_state (Gst.State.PLAYING)
+	def stop(self):
+		self.player.set_state(Gst.State.NULL)
+
+	
+	def get_pipeline(self):
+		return Gst.parse_launch('filesrc location={0} ! '.format(self._filepath) + 'decodebin ! autovideosink')
 
 
 	def on_bus_msg(self, bus, msg):
 		if msg is None:
 			return
+		elif msg.type is Gst.MessageType.SEGMENT_DONE: 
+			self.player.seek_simple(Gst.Format.TIME, Gst.SeekFlags.SEGMENT, 0)
+		elif msg.type is Gst.MessageType.EOS:
+			self.player.seek_simple(Gst.Format.TIME, Gst.SeekFlags.FLUSH, 0)
+		elif msg.type is Gst.MessageType.ASYNC_DONE:
+			if self._prerolling:
+				print "prerolling"
+				#self.player.seek_simple(Gst.Format.TIME, Gst.SeekFlags.FLUSH | Gst.SeekFlags.SEGMENT, 0)
+				self.player.seek_simple(Gst.Format.TIME, Gst.SeekFlags.FLUSH, 0)
+				self.player.set_state(Gst.State.PLAYING)
+				self._prerolling = False
 		elif msg.type is Gst.MessageType.ERROR:
 			print "Got message of type ", msg.type
 			print "Got message of src ", msg.src
 			print "Got message of error ", msg.parse_error()
 			self.player.set_state(Gst.State.NULL)
 
- 
+
 	def about_to_finish(self, playbin):
+		print "about to finish"
 		if self._loop:
 			print "adding to loop"
-			self.player.props.uri = self._last_uri 
+	                self.player.props.uri = self._last_uri
 
 
 	@property	
@@ -53,34 +73,48 @@ class Player(object):
 		self._loop = value
 
 
-	@property
-	def slave(self):
-		return self._slave
-
-
-	def set_slave(self, ip, port, base_time):
-		self.player.set_start_time(self.GST_CLOCK_TIME_NONE)
-		self._clock = GstNet.NetClientClock.new("clock", ip, port, base_time)
-		self.player.set_base_time(base_time)
+class MasterPlayer(Player):
+	def __init__(self, filepath, port):
+		super(MasterPlayer, self).__init__(filepath)
+		
+		self._clock = self.player.get_clock()
 		self.player.use_clock(self._clock)
+		self._clock_provider = GstNet.NetTimeProvider.new(self._clock, None, port)
+		self._base_time = self._clock.get_time()
+		self.player.set_start_time(self.GST_CLOCK_TIME_NONE)
+		self.player.set_base_time(self._base_time)
 
-		self._slave = True
+	@property
+	def base_time(self):
+		return self._base_time
 
 
-	def set_master(self, port):
-	    	self._clock = self.player.get_clock()
-    		self.player.use_clock(self._clock)
-
-    		self._clock_provider = GstNet.NetTimeProvider.new(self._clock, None, port)
-
-    		base_time = self._clock.get_time()
-
-    		self.player.set_start_time(self.GST_CLOCK_TIME_NONE)
-    		self.player.set_base_time(base_time)
-		
-		self._master = True
-		
-		return base_time
+class SlavePlayer(Player):
+	def __init__(self, filepath, ip, port, base_time):
+		super(SlavePlayer, self).__init__(filepath)
+		self._ip = ip
+        	self._port = port
+        	self._base_time = base_time
 
 	
+	@property
+	def base_time(self):
+		return self._base_time
+
+
+	@base_time.setter
+	def base_time(self, value):
+		self.player.set_start_time(self.GST_CLOCK_TIME_NONE)
+                self._base_time = value
+                self._clock = Gst.NetClientClock(None, self._ip, self._port, self._base_time)
+                self.player.set_base_time(self._base_time)
+                self.player.use_clock(self._clock)
+
+
+if __name__ == '__main__':
+	from gi.repository import GObject
+
+	player = MasterPlayer(sys.argv[1], 11111)
+
+	GObject.MainLoop().run()
 
