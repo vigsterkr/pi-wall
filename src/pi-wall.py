8a94faac52d1859c0ce6116aef72946d62ec4667
Viktor Gal <viktor.gal@maeth.com>
2014-02-06 11:51:47 +0000

    Add config support and start-stop script for debian

---
+++ src/pi-wall.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+from player import * 
+from gi.repository import GObject
+from networking import *
+import json
+
+if __name__ == "__main__":
+	try:
+		f = open('/etc/pi-wall.conf')
+	except IOError:
+		print "cannot open config file"
+	else:
+		config = json.load(f)
+		f.close()
+	
+		if config['type'] == 'master':
+			player = MasterPlayer(config['movie_file'], int(config['master_port'))
+			ms = MasterServerThread(player, config['bcast_addr'])
+			ms.start()
+
+			try:
+				GObject.MainLoop().run()
+			except KeyboardInterrupt:
+				ms.stop()
+				sys.exit(1)
+		elif config['type'] == 'slave':
+			slave = SlaveThread(config['movie_file'], config['bcast_addr'])
+			slave.start()
+
+			try:
+				GObject.MainLoop().run()
+			except KeyboardInterrupt:
+				slave.stop_player()
+				sys.exit(1)
+		else:
+			print "error type"
+
+
