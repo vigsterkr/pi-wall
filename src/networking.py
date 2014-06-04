edb1a75953713694f8023e4e554ce7b4913f11f1
Viktor Gal <viktor.gal@maeth.com>
2014-06-04 15:59:50 +0200

    Fix pep8 errors add audio_sink property for Player and set it to pulsesink by default

--- src/networking.py
+++ src/networking.py
@@ -6,57 +6,54 @@ from socket import *
 import threading
 from player import *
 
-UDP_PORT = 31337 
+UDP_PORT = 31337
+
 
 class MasterServerThread(threading.Thread):
     def __init__(self, master_player, bcast_addr):
         super(MasterServerThread, self).__init__()
         self.master_player = master_player
-	self.sock = socket(AF_INET, SOCK_DGRAM)
+        self.sock = socket(AF_INET, SOCK_DGRAM)
         self.sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         self.sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-	self.bcast_addr = bcast_addr
-	self.running = True
+        self.bcast_addr = bcast_addr
+        self.running = True
 
     def run(self):
-	while self.running:
-		msg = struct.pack("!Qi", self.master_player.base_time, self.master_player.port)
-        	self.sock.sendto(msg, (self.bcast_addr, UDP_PORT))
-        	time.sleep(1)
+        while self.running:
+            msg = struct.pack("!Qi", self.master_player.base_time,
+                              self.master_player.port)
+            self.sock.sendto(msg, (self.bcast_addr, UDP_PORT))
+            time.sleep(1)
 
     def stop(self):
-	self.master_player.stop()
-	self.running = False
+        self.master_player.stop()
+        self.running = False
 
 
 class SlaveThread(threading.Thread):
-	def __init__(self, filepath, bcast_addr):
-		super(SlaveThread, self).__init__()
-		self.filepath = filepath
+    def __init__(self, filepath, bcast_addr):
+        super(SlaveThread, self).__init__()
+        self.filepath = filepath
+        self.slave = None
+        self.running = True
+        self.sock = socket(AF_INET, SOCK_DGRAM)
+        self.sock.bind((bcast_addr, UDP_PORT))
 
-        	self.slave = None
-        	self.running = True
-		
-		self.sock = socket(AF_INET, SOCK_DGRAM)
-		self.sock.bind((bcast_addr, UDP_PORT))
-	
+    def run(self):
+        while self.running:
+            data, addr = self.sock.recvfrom(100)
+            base_time, port = struct.unpack('!Qi', data)
+            ip = addr[0]
+#           print base_time, ip, port
 
-	def run(self):
-		while self.running:
-			data, addr = self.sock.recvfrom(100)
-			base_time, port = struct.unpack('!Qi', data)
-			ip = addr[0]
-#			print base_time, ip, port
-			
-			if self.slave is None:
-				self.slave = SlavePlayer(self.filepath, ip, port, base_time) 
-			elif base_time != self.slave.base_time:
-				print("base time does not match, restarting...")
-				self.slave.stop()
-				self.slave = SlavePlayer(self.filepath, ip, port, base_time)
+            if self.slave is None:
+                self.slave = SlavePlayer(self.filepath, ip, port, base_time)
+            elif base_time != self.slave.base_time:
+                print("base time does not match, restarting...")
+                self.slave.stop()
+                self.slave = SlavePlayer(self.filepath, ip, port, base_time)
 
-
-	def stop_player(self):
-		self.slave.stop()
-        	self.running = False
-
+    def stop_player(self):
+        self.slave.stop()
+        self.running = False
--- src/player.py
+++ src/player.py
@@ -7,122 +7,132 @@ from gi.repository import Gst, GstNet, GObject
 GObject.threads_init()
 Gst.init(None)
 
+
 class Player(object):
-	def __init__(self, filepath):
-		self._filepath = filepath
-		self.player = self.get_pipeline()
+    def __init__(self, filepath):
+        self._filepath = filepath
+        self.player = self.get_pipeline()
 
-		self.bus = self.player.get_bus()
-		self.bus.add_signal_watch()
-	        self.bus.connect("message", self.on_bus_msg)
-		
-		self._loop = False
-		
-		self.player.set_state(Gst.State.PAUSED)
-		
-		# bug ? as i get -1 for the standard
-		self.GST_CLOCK_TIME_NONE = 18446744073709551615
+        self.bus = self.player.get_bus()
+        self.bus.add_signal_watch()
+        self.bus.connect("message", self.on_bus_msg)
 
-		self._prerolling = True
+        self._loop = False
 
-	
-	def stop(self):
-		self.player.set_state(Gst.State.NULL)
+        self.player.set_state(Gst.State.PAUSED)
 
-	
-	def get_pipeline(self):
-		return Gst.parse_launch('filesrc location={0} ! '.format(self._filepath) + 'decodebin name=demux demux. ! eglglessink demux. ! alsasink')
+        # bug ? as i get -1 for the standard
+        self.GST_CLOCK_TIME_NONE = 18446744073709551615
 
+        self._prerolling = True
+        self._audio_sink = 'pulsesink'
 
-	def on_bus_msg(self, bus, msg):
-		if msg is None:
-			return
-		elif msg.type is Gst.MessageType.SEGMENT_DONE: 
-			self.player.seek_simple(Gst.Format.TIME, Gst.SeekFlags.SEGMENT, 0)
-		elif msg.type is Gst.MessageType.ASYNC_DONE:
-			if self._prerolling:
-				self.player.seek_simple(Gst.Format.TIME, Gst.SeekFlags.FLUSH | Gst.SeekFlags.SEGMENT, 0)
-				self.player.set_state(Gst.State.PLAYING)
-				self._prerolling = False
-		elif msg.type is Gst.MessageType.ERROR:
-			print "Got message of type ", msg.type
-			print "Got message of src ", msg.src
-			print "Got message of error ", msg.parse_error()
-			self.player.set_state(Gst.State.NULL)
+    def stop(self):
+        self.player.set_state(Gst.State.NULL)
 
+    def get_pipeline(self):
+        return Gst.parse_launch('filesrc location={0} ! '
+                                .format(self._filepath)
+                                + 'decodebin name=demux demux. \
+                                ! eglglessink demux. ! ' + self.audio_sink)
 
-	def about_to_finish(self, playbin):
-		print "about to finish"
-		if self._loop:
-			print "adding to loop"
-	                self.player.props.uri = self._last_uri
+    def on_bus_msg(self, bus, msg):
+        if msg is None:
+            return
+        elif msg.type is Gst.MessageType.SEGMENT_DONE:
+            self.player.seek_simple(Gst.Format.TIME, Gst.SeekFlags.SEGMENT, 0)
+        elif msg.type is Gst.MessageType.ASYNC_DONE:
+            if self._prerolling:
+                self.player.seek_simple(Gst.Format.TIME,
+                                        Gst.SeekFlags.FLUSH
+                                        | Gst.SeekFlags.SEGMENT, 0)
+                self.player.set_state(Gst.State.PLAYING)
+                self._prerolling = False
+        elif msg.type is Gst.MessageType.ERROR:
+            print "Got message of type ", msg.type
+            print "Got message of src ", msg.src
+            print "Got message of error ", msg.parse_error()
+            self.player.set_state(Gst.State.NULL)
 
+    def about_to_finish(self, playbin):
+        print "about to finish"
+        if self._loop:
+            print "adding to loop"
+            self.player.props.uri = self._last_uri
 
-	@property	
-	def loop(self):
-		return self._loop 
+    @property
+    def loop(self):
+        return self._loop
 
+    @loop.setter
+    def loop(self, value):
+        self._loop = value
 
-	@loop.setter
-	def loop(self, value):
-		self._loop = value
+    @property
+    def audio_sink(self):
+        return self._audio_sink
+
+    @audio_sink.setter
+    def audio_sink(self, value):
+        self._audio_sink = value
 
 
 class MasterPlayer(Player):
-	def __init__(self, filepath, port):
-		super(MasterPlayer, self).__init__(filepath)
-		
-		self._port = port
-		self._clock = self.player.get_clock()
-		self.player.use_clock(self._clock)
-		self._clock_provider = GstNet.NetTimeProvider.new(self._clock, None, self._port)
-		self._base_time = self._clock.get_time()
-		self.player.set_start_time(self.GST_CLOCK_TIME_NONE)
-		self.player.set_base_time(self._base_time)
+    def __init__(self, filepath, port):
+        super(MasterPlayer, self).__init__(filepath)
 
-	@property
-	def base_time(self):
-		return self._base_time
+        self._port = port
+        self._clock = self.player.get_clock()
+        self.player.use_clock(self._clock)
+        self._clock_provider = GstNet.NetTimeProvider.new(self._clock,
+                                                          None,
+                                                          self._port)
+        self._base_time = self._clock.get_time()
+        self.player.set_start_time(self.GST_CLOCK_TIME_NONE)
+        self.player.set_base_time(self._base_time)
 
+    @property
+    def base_time(self):
+        return self._base_time
 
-	@property
-	def port(self):
-		return self._port
+    @property
+    def port(self):
+        return self._port
 
 
 class SlavePlayer(Player):
-	def __init__(self, filepath, ip, port, base_time):
-		super(SlavePlayer, self).__init__(filepath)
-		self._ip = ip
-        	self._port = port
-		self.base_time = base_time
-	
+    def __init__(self, filepath, ip, port, base_time):
+        super(SlavePlayer, self).__init__(filepath)
+        self._ip = ip
+        self._port = port
+        self.base_time = base_time
 
-	@property
-	def base_time(self):
-		return self._base_time
+    @property
+    def base_time(self):
+        return self._base_time
 
-
-	@base_time.setter
-	def base_time(self, value):
-		self.player.set_start_time(self.GST_CLOCK_TIME_NONE)
-                self._base_time = value
-                self._clock = GstNet.NetClientClock.new("clock", self._ip, self._port, self._base_time)
-                self.player.set_base_time(self._base_time)
-                self.player.use_clock(self._clock)
+    @base_time.setter
+    def base_time(self, value):
+        self.player.set_start_time(self.GST_CLOCK_TIME_NONE)
+        self._base_time = value
+        self._clock = GstNet.NetClientClock.new("clock",
+                                                self._ip,
+                                                self._port,
+                                                self._base_time)
+        self.player.set_base_time(self._base_time)
+        self.player.use_clock(self._clock)
 
 
 if __name__ == '__main__':
-	import sys
-	print sys.argv[1]
+    import sys
+    print sys.argv[1]
 
-	if sys.argv[1] == 'master':
-		player = MasterPlayer(sys.argv[2], 11111)
-		print("base_time={0}".format(player.base_time))
-	elif sys.argv[1] == 'slave':
-		player = SlavePlayer(sys.argv[2], sys.argv[3], 11111, int(sys.argv[4]))
-	else:
-		player = Player(sys.argv[1])
+    if sys.argv[1] == 'master':
+        player = MasterPlayer(sys.argv[2], 11111)
+        print("base_time={0}".format(player.base_time))
+    elif sys.argv[1] == 'slave':
+        player = SlavePlayer(sys.argv[2], sys.argv[3], 11111, int(sys.argv[4]))
+    else:
+        player = Player(sys.argv[1])
 
-	GObject.MainLoop().run()
-
+    GObject.MainLoop().run()
